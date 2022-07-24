afc1d316550deac7d8d4946544fb17ba095267d3
Mohamed2221 <87386058+mohamedhamdy12@users.noreply.github.com>
2022-07-24 12:50:12 +0200

    teranslate police

--- src/bages/Police.js
+++ src/bages/Police.js
@@ -6,39 +6,58 @@ const Police = () => {
  const { t, i18n } = useTranslation();
  return (
   <MainSectionThre>
-   <Container>
+   <Container maxWidth="xl">
     <ul>
      <h2>{t("police.title1")}</h2>
-     <p>JM HOLDING Company LLC, trading as JM HOLDING (“the Company”) is incorporated in Kuwait registered
-      address at Kuwait City – Qibla – Safat Tower – 26 Floor.
-      JM HOLDING is committed to the highest standards of the Anti-Money Laundering (AML) compliance and
-      Counter-Terrorism Financing (CTF). To help the governments first and the financial services industry fight the
-      funding of terrorism and money laundering activities, law requires all financial institutions to obtain, verify, and
-      record information that identifies each person opening an account.
-     </p>
-     <h2>General</h2>
-     <p>Money laundering – the process of converting funds, received from illegal activities (such as fraud, corruption,
-      terrorism, etc.), into other funds or investments that look legitimate to hide or distort the real source of funds.
-      The process of money laundering can be divided into three sequential stages:</p>
-     <li>Placement: At this stage, funds are converted into financial instruments, such as checks, bank accounts,
-      and money transfers, or can be used for purchasing high-value goods that can be resold. They can also
-      be physically deposited into banks and non-bank institutions (e.g., currency exchangers). To avoid
-      suspicion by the company, the launderer may as well make several deposits instead of depositing the
-      whole sum at once, this form of placement is called smurfing.
-      </li>
-     <li>Layering: Funds are transferred or moved to other accounts and other financial instruments. It is
-      performed to disguise the origin and disrupt the indication of the entity that made the multiple financial
-      transactions. Moving funds around and changing in their form makes it complicated to trace the money
-      being laundered.
-      </li>
-     <li>Integration: Funds get back into circulation as legitimate to purchase goods and services</li>
-     <p>JM HOLDING adheres to the principles of Anti-Money Laundering and actively prevents any actions that aim
-      or facilitate the process of legalizing of illegally gained funds. AML policy means preventing the use of the
-      company’s services by criminals, with the aim of money laundering, terrorist financing or other criminal activity.
-      To prevent money laundering, JM HOLDING neither accepts nor pays cash under any circumstances. The
-      company reserves the right to suspend any client’s operation, which can be regarded as illegal or, may be related
-      to money laundering in the opinion of the staff.
-      </p>
+     <p>{t("police.line1")}</p>
+     <h2>{t("police.title2")}</h2>
+     <p>{t("police.line2")}</p>
+     <li>{t("police.li1")} </li>
+     <li> {t("police.li2")}</li>
+     <li>{t("police.li3")}</li>
+     <p>{t("police.line3")}</p>
+     <h2>{t("police.title3")}</h2>
+     <p>{t("police.line4")}</p>
+     <li> {t("police.li4")}</li>
+     <li> {t("police.li5")}</li>
+     <li> {t("police.li6")}</li>
+     <p>{t("police.line5")}</p>
+     <h2>{t("police.title4")}</h2>
+     <p>{t("police.line6")}</p>
+     <li> {t("police.li7")}</li>
+     <li> {t("police.li8")}</li>
+     <li> {t("police.li9")}</li>
+     <li> {t("police.li10")}</li>
+
+     <h2>{t("police.title5")}</h2>
+     <p>{t("police.line7")}</p>
+     <li> {t("police.li11")}</li>
+     <li> {t("police.li12")}</li>
+     <li> {t("police.li13")}</li>
+     <li> {t("police.li14")}</li>
+     <li> {t("police.li15")}</li>
+     <li> {t("police.li16")}</li>
+     <h2>{t("police.title6")}</h2>
+     <p>{t("police.line8")}</p>
+     <h2>{t("police.title7")}</h2>
+     <p>{t("police.line9")}</p>
+
+     <h2>{t("police.title8")}</h2>
+     <p>{t("police.line10")}</p>
+     <li> {t("police.li17")}</li>
+     <li> {t("police.li18")}</li>
+     <li> {t("police.li19")}</li>
+     <li> {t("police.li120")}</li>
+     <h2>{t("police.title9")}</h2>
+     <li> {t("police.li21")}</li>
+     <li> {t("police.li22")}</li>
+     <li> {t("police.li23")}</li>
+     <li> {t("police.li24")}</li>
+     <h2>{t("police.title10")}</h2>
+     <li> {t("police.li25")}</li>
+     <li> {t("police.li26")}</li>
+
+
     </ul>
    </Container>
   </MainSectionThre>
@@ -48,5 +67,13 @@ const MainSectionThre = styled.div`
 position: relative;
 transform: translateY(-70px);
 margin: 80px 5px 20px 5px;
+color: white;
+h2{
+ color: var(--primary-color);
+}
+li{
+ margin: 0 30px;
+ padding: 5px 0;
+}
 `
 export default Police
